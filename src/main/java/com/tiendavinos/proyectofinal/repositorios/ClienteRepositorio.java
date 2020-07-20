e206255cb5fa92a241a41bf41a089d5ffcc1cafc
Juani-Ortizz <65369113+Juani-Ortizz@users.noreply.github.com>
2020-07-20 19:39:34 -0300

    validar registro

--- src/main/java/com/tiendavinos/proyectofinal/repositorios/ClienteRepositorio.java
+++ src/main/java/com/tiendavinos/proyectofinal/repositorios/ClienteRepositorio.java
@@ -3,9 +3,14 @@ package com.tiendavinos.proyectofinal.repositorios;
 
 import com.tiendavinos.proyectofinal.entidades.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClienteRepositorio extends JpaRepository<Cliente, String>{
+    
+    @Query("SELECT c FROM Cliente c WHERE c.email = :email")
+    public Cliente buscarPorMail(@Param("email")String email);
     
 }
