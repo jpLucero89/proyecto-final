2da03288a122871b9adebcd2c8064b33f7e6bc67
VictorHAguilera <viktotem@gmail.com>
2020-08-04 13:52:14 -0300

    agrego vistas exito

--- src/main/java/com/tiendavinos/proyectofinal/controladores/PortalControlador.java
+++ src/main/java/com/tiendavinos/proyectofinal/controladores/PortalControlador.java
@@ -134,7 +134,8 @@ public class PortalControlador {
     public String cargarProveedor(ModelMap modelo) {
         return "cargarProveedor";
     }
-
+    
+    
     //////////////////////////////////////////////////////////////////
     
       
